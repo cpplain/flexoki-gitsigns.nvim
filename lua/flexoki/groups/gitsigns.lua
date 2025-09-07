26ade540e53fd32b3475319ef26b52c8872e563c
Christopher Plain <me@christopherplain.com>
2025-09-06 21:11:56 -0700

    style: remove base highlights and add staged (#3)
    - Removed redundant base highlights that are correct by default
    - Add staged highlights to use correct Flexoki palette colors

--- lua/flexoki/groups/gitsigns.lua
+++ lua/flexoki/groups/gitsigns.lua
@@ -5,9 +5,31 @@ local M = {}
 ---@return flexoki.HighlightGroup
 function M.get(colors)
     return {
-        GitSignsAdd = { fg = colors.green },
-        GitSignsChange = { fg = colors.yellow },
-        GitSignsDelete = { fg = colors.red },
+        GitSignsDeleteLn = { link = "DiffDelete" },
+        GitSignsStagedAdd = { fg = colors.green3 },
+        GitSignsStagedChange = { fg = colors.yellow3 },
+        GitSignsStagedDelete = { fg = colors.red3 },
+        GitSignsStagedChangedelete = { link = "GitSignsStagedChange" },
+        GitSignsStagedTopdelete = { link = "GitSignsStagedDelete" },
+        GitSignsStagedUntracked = { link = "GitSignsStagedAdd" },
+        GitSignsStagedAddNr = { link = "GitSignsStagedAdd" },
+        GitSignsStagedChangeNr = { link = "GitSignsStagedChange" },
+        GitSignsStagedDeleteNr = { link = "GitSignsStagedDelete" },
+        GitSignsStagedChangedeleteNr = { link = "GitSignsStagedChange" },
+        GitSignsStagedTopdeleteNr = { link = "GitSignsStagedDelete" },
+        GitSignsStagedUntrackedNr = { link = "GitSignsStagedAdd" },
+        GitSignsStagedAddLn = { fg = colors.green3, bg = colors.green4 },
+        GitSignsStagedChangeLn = { fg = colors.yellow3, bg = colors.yellow4 },
+        GitSignsStagedDeleteLn = { fg = colors.red3, bg = colors.red4 },
+        GitSignsStagedChangedeleteLn = { link = "GitSignsStagedChangeLn" },
+        GitSignsStagedTopdeleteLn = { link = "GitSignsStagedDeleteLn" },
+        GitSignsStagedUntrackedLn = { link = "GitSignsStagedAddLn" },
+        GitSignsStagedAddCul = { link = "GitSignsStagedAdd" },
+        GitSignsStagedChangeCul = { link = "GitSignsStagedChange" },
+        GitSignsStagedDeleteCul = { link = "GitSignsStagedDelete" },
+        GitSignsStagedChangedeleteCul = { link = "GitSignsStagedChange" },
+        GitSignsStagedTopdeleteCul = { link = "GitSignsStagedDelete" },
+        GitSignsStagedUntrackedCul = { link = "GitSignsStagedAdd" },
     }
 end
 
